9d3e42869c33a1872ea4c6f11b13e1ffe9636901
feynoobs <p.w.feeniej@gmail.com>
2023-12-13 19:33:01 +0900

    goggo

--- routes/web.php
+++ routes/web.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 // Route::get('/', function () {
 //     return view('index');
 // });
-Route::get('/{any?}', fn() => view('app')) -> where('any', '.+');
+Route::get('/{any?}', fn() => view('app'))->where('any', '.+');
